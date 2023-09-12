bb84e2274977273dba11ea245da5c91bc53d1c0c
millamzy <ndefoago@gmail.com>
2023-09-12 15:11:47 +0100

    final commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://user-login-server.onrender.com"],
     methods:["GET","POST"],
     credentials:true
 }));
